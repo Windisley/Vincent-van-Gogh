57f56ff42eff3efa8b2187eee78b4ebcdf3994e1
Bianca <164269876+biancaalvess@users.noreply.github.com>
2025-08-03 00:17:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Adicione a classe 'page-loaded' ao elemento <body>:
   document.body.classList.add("page-loaded")
 })
-
-console.log("Página de Arte de Van Gogh carregada!")
