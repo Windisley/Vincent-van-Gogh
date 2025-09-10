2cc127abf182504858eeea8408080b256dd0ba36
Windisley <windisleyl@gmail.com>
2025-09-09 23:36:01 -0300

    Logica Do Menu Lateral Pronta

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("page-loaded")
+
+  const buttomMenu = document.querySelector(".box-button-menu")
+  const headerMobile = document.querySelector(".header-mobile")
+  buttomMenu.addEventListener("click", ()=>{
+    headerMobile.classList.toggle("header-mobile-responsive")
+  })
+
 })
